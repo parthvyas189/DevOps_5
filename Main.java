cf0458eacb18490091adebe85dc5d01eada6068c
parthvyas189 <125575565+parthvyas189@users.noreply.github.com>
2025-09-08 15:12:33 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Updated message after modification!");
+        System.out.println("Modification #2!");
 
     }
 }
